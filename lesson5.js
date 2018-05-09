b6006f2a0ec9dce18d3b5562496692586e3ef442
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-08 23:14:20 -0400

    Lesson5

---
+++ lesson5.js
@@ -0,0 +1,21 @@
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book)){
+    console.log(book.textContent);
+});
+
+Array.from(books).forEach(function(book){
+    book.textContent += 'test';
+});
+
+
+//fixed it!
+Array.from(books).forEach(function(book){
+    book.textContent = book.textContent.substring(0,book.textContent.length - 4);
+});
+
+const bookList = document.querySelector('#book-list');
+console.log(bookList.innerHTML);
+
+
+bookList.innerHTML = '<h2>Books and more books...</h2>';
--- querySelector.js
+++ querySelector.js
@@ -2,12 +2,12 @@ $('#wrapper');
 //OR document.querySelector('wrapper');
 
 const wrap = document.querySelector('#wrapper');
-#console.log(wrap);
+//console.log(wrap);
 
-const wmf = document.querySelector(#book-list li:nth-chile(2).name');
+const wmf = document.querySelector(#book-list li:nth-child(2).name');
 
 var books = document.querySelector('#book-list li.name');
-#console.log(books);
+//console.log(books);
 
 books = document.querySelectorAll('#book-list li.name');
 console.log(books);
