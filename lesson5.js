bafd96708d52e639155b5f2ddd9646450727374b
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-10 19:35:47 -0400

    Lesson6 Completed

--- lesson5.js
+++ lesson5.js
@@ -15,8 +15,9 @@ Array.from(books).forEach(function(book){
 });
 
 const bookList = document.querySelector('#book-list');
-console.log(bookList.innerHTML);
+//console.log(bookList.innerHTML);
 
 
 bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML = '<p>This is how you add HTML<p>';
 
