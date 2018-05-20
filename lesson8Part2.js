972407848bd5d5e0efd9f44a0ff09a6c53a62c18
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-20 14:46:44 -0400

    Lesson8 Traversing Part 2

---
+++ lesson8Part2.js
@@ -0,0 +1,11 @@
+const bookList = document.querySelector(#book-list);
+
+console.log('book-list next sibling is:', bookList.nextSibling);
+console.log('book-list next element sibling is:', bookList.nextElementSibling);
+
+
+console.log('book-list previous sibling is:', bookList.previousSibling);
+console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!';
