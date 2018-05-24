8b8ee2b7cfb263512ee9f8155d35703037f4a276
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-23 22:52:53 -0400

    Lesson 14

---
+++ lesson14.js
@@ -0,0 +1,19 @@
+var book = document.querySelector('li:first-child .name');
+
+book.getAttribute('class');
+
+console.log(book);
+
+console.log(book.getAttribute('class'));
+
+console.log(book.getAttribute('href'));
+
+console.log(book.setAttribute('class', 'name-2'))
+
+console.log(book);
+
+console.log(boo.hasAttribute('class'));
+
+console.log(book.hasAttribute('false'));
+
+book.removeAttribute('class');
