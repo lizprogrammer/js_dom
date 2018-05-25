ed5d02c514ac8d594563ae1e920602654da2a325
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-25 15:22:32 -0400

    Lesson 17

--- lesson17.js
+++ lesson17.js
@@ -1,78 +1,77 @@
-// Delete books
-const list = document.querySelector('#book-list ul');
-
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
+const list=document.querySelector('#book-list ul');
+//delete books
+list.addEventListener('click',function(e){
+  if (e.target.className == 'delete'){
+    const li=e.target.parentElement;
+    // li.parentNode.removeChild(li);
+       list.removeChild(li);
+  }
 });
 
-// Add books
-// document.forms
-
-const addForm = document.forms['add-book'];
-
+//add book
+const addForm=document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
-	e.preventDefault();
-	const value = addForm.querySelector('input[type = "text"]').value
-	console.log(value);
+  e.preventDefault();
+  const value=addForm.querySelector('input[Type="text"]').value;
 
-	// create elements
-	const li = document.createElement('li');
-	const bookName = document.createElement('span');
-	const deleteBtn = document.createElement('button');
+  // create elements
+  const li = document.createElement('li');
+  const bookName=document.createElement('span');
+  const deleteBtn=document.createElement('span');
 
-    // add content
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = value;
+  //add content
+  deleteBtn.textContent ='delete';
+  bookName.textContent= value;
 
-	// append to document
-	li.appendChild(bookName);
-	li.appendChild(deleteBtn);
-	list.appendChild(li);
+  //add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
 
-});
-
-// hide books
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-    if(hideBox.checked){
-        list.style.display = "none";
-
-    }
-    else{
-         list.style.display = "initial";
-    }
+  // append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 
 });
 
-//filter books
-const searchBar = document.forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', function(e){
-    const term = e.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    Array.from(books).forEach(function(book){
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(term) >= 0  ){
-            book.style.display = 'block';
-        }
-        else{
-            book.style.display = 'none';
-        }
 
-    })
+// Hide books
+const hideBox=document.querySelector('#hide');
+hideBox.addEventListener('change',function(e){
+  if (hideBox.checked){
+    list.style.display="none";
+  }else{
+    list.style.display="initial";
+  }
+});
+
+//filter Books
+const searchBar=document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup',function(e){
+  const term=e.target.value.toLowerCase();
+  const books=list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book){
+    const title=book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term)!=-1){
+      book.style.display='block';
+    } else {
+      book.style.display = 'none';
+    }
+  })
 });
 
 // tabbed content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
+const tabs =document.querySelector('.tabs');
+const panels =document.querySelectorAll('.panel');
 tabs.addEventListener('click', function(e){
-    if(e.target.tabName = "LI"){
-        const targetPanel = document.querySelector(e.target.dataset.target);
-    }
-})
-
-
-
-
+  if(e.target.tagName == "LI"){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach(function(panel){
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
